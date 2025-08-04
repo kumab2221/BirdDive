509592fe15e6a1e9ee946d551002c09201e87570
kumab2221 <kumab2221@gmail.com>
2025-08-04 17:48:40 +0900

    BirdDive/Plugins/UnrealMCPで動作確認済環境

---
+++ BirdDive/Source/BirdDive/BirdDiveDummy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "BirdDiveDummy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIRDDIVE_API UBirdDiveDummy : public UObject
+{
+	GENERATED_BODY()
+	
+};
